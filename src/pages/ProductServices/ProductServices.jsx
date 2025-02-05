13afadbd663307b600eb651ecbea85a911e5bd97
Shah Arefin Ahmed <shaharefinahmed@gmail.com>
2025-02-05 12:56:10 +0600

    File structure has been setup with pages

---
+++ src/pages/ProductServices/ProductServices.jsx
@@ -0,0 +1,9 @@
+const ProductServices = () => {
+  return (
+    <div>
+      <h2>This is Product & Services Page</h2>
+    </div>
+  );
+};
+
+export default ProductServices;
---
+++ src/routes/Router.jsx
@@ -0,0 +1,26 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../layouts/Main";
+import Home from "../pages/Home/Home/Home";
+import AboutUs from "../pages/AboutUs/AboutUs";
+import ProductServices from "../pages/ProductServices/ProductServices";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/aboutUs",
+        element: <AboutUs />,
+      },
+      {
+        path: "/productServices",
+        element: <ProductServices />,
+      },
+    ],
+  },
+]);
