0f4c94f1e56a0a1f52cff49aa041d7a0078887d2
Shah Arefin Ahmed <shaharefinahmed@gmail.com>
2025-02-11 18:18:22 +0600

    Have and Need page almost completed

--- src/components/DesignInformation/HaveDesign/HaveDesign.jsx
+++ src/components/DesignInformation/HaveDesign/HaveDesign.jsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
 import { UploadFileSvg } from "../../SvgContainer/SvgConainer";
+import Button from "../../Button/Button";
 
 const HaveDesign = () => {
   const [selectedFile, setSelectedFile] = useState(null);
-
+  const [isToggled, setIsToggled] = useState(false);
   // Handle file selection
   const handleFileChange = (event) => {
     const file = event.target.files[0]; // Get the first selected file
@@ -11,6 +12,12 @@ const HaveDesign = () => {
       setSelectedFile(file);
     }
   };
+
+  // Toggle the state when switch is clicked
+  const handleToggle = () => {
+    setIsToggled((prevState) => !prevState);
+  };
+
   return (
     <section>
       {/*  */}
@@ -40,7 +47,7 @@ const HaveDesign = () => {
             *Accept PNG, JPG, PDF, AI, SVG (max size: 5MB).
           </p>
           {/* This is the choose file section */}
-          <div className="bg-[#FFF] shadow-fileUpload py-4 rounded-[10px]">
+          <div className="bg-[#FFF] shadow-fileUpload py-4 rounded-[10px] mt-4">
             <div className="flex justify-center items-center">
               <label className="cursor-pointer px-4 py-2 text-headingColor text-base rounded-lg text-center font-semibold">
                 Choose File
@@ -53,12 +60,288 @@ const HaveDesign = () => {
               <UploadFileSvg />
             </div>
             {selectedFile && (
-              <p className="text-gray-700">
+              <p className="text-navbarColor text-center">
                 Selected File: {selectedFile.name}
               </p>
             )}
           </div>
+          {/* This is the preferred color */}
+          <div>
+            {/* First Input Column */}
+            <div className="flex gap-10 mt-[19px]">
+              <div className="flex flex-col gap-y-[10.5px] w-[50%]">
+                <label className="text-lg text-headingColor font-medium">
+                  Preferred Colors*
+                </label>
+                <select
+                  className="py-[28px] pl-[20px] bg-[#D9D9D91A] rounded-[10px] border border-gray-400"
+                  defaultValue="" // ✅ Proper way to set default value
+                >
+                  <option value="" disabled>
+                    Choose an option
+                  </option>
+                  <option value="White">White</option>
+                  <option value="Black">Black</option>
+                  <option value="Blue">Blue</option>
+                </select>
+              </div>
+
+              {/* Preferred Finish (optional) */}
+              <div className="flex flex-col gap-y-[10.5px] w-[50%]">
+                <label className="text-lg text-headingColor font-medium">
+                  Preferred Finish (optional)
+                </label>
+                <select
+                  className="py-[28px] pl-[20px] bg-[#D9D9D91A] rounded-[10px] border border-gray-400"
+                  defaultValue=""
+                >
+                  <option value="" disabled>
+                    Choose an option
+                  </option>
+                  <option value="Matte">Matte</option>
+                  <option value="Glossy">Glossy</option>
+                  <option value="Satin">Satin</option>
+                </select>
+              </div>
+            </div>
+            {/* Second Input Column  */}
+            <div className="flex gap-10 mt-[19px]">
+              <div className="flex flex-col gap-y-[10.5px] w-[50%]">
+                <label className="text-lg text-headingColor font-medium">
+                  Phone*
+                </label>
+                <input
+                  className="py-[31px] pl-[49px] bg-[#D9D9D91A] rounded-[10px]"
+                  type="number"
+                  placeholder="Phone"
+                />
+              </div>
+              {/* Brand Text  */}
+              <div className="flex flex-col gap-y-[10.5px] w-[50%]">
+                <label className="text-lg text-headingColor font-medium">
+                  Brand Text
+                </label>
+                <input
+                  className="py-[31px] pl-[49px] bg-[#D9D9D91A] rounded-[10px]"
+                  type="text"
+                  placeholder="Enter text for your packaging (e.g., brand name, tagline)"
+                />
+              </div>
+            </div>
+            {/*  */}
+            <div className="bg-[#FFF] shadow-fileUpload py-4 rounded-[10px] mt-4 max-w-[664px]">
+              <div className="flex justify-center items-center">
+                <label className="cursor-pointer px-4 py-2 text-headingColor text-base rounded-lg text-center font-semibold">
+                  Choose File
+                  <input
+                    type="file"
+                    className="hidden"
+                    onChange={handleFileChange}
+                  />
+                </label>
+                <UploadFileSvg />
+              </div>
+              {selectedFile && (
+                <p className="text-navbarColor text-center">
+                  Selected File: {selectedFile.name}
+                </p>
+              )}
+            </div>
+            {/* This is the Design Placement */}
+            <div className="flex flex-col gap-y-[10.5px] mt-[15.5px]">
+              <label className="text-lg text-headingColor font-medium">
+                Design Placement:*
+              </label>
+              <input
+                className="py-[31px] pl-[49px] bg-[#D9D9D91A] rounded-[10px]"
+                type="number"
+                placeholder="Design Placement"
+              />
+            </div>
+
+            {/* Product Information */}
+            {/* This is the Product Information */}
+            <div className="mt-12">
+              <h1 className="text-center text-buttonColor text-2xl">
+                Product Information
+              </h1>
+              {/* First Input Column */}
+              <div className="flex gap-10 mt-[19px]">
+                <div className="flex flex-col gap-y-[10.5px] w-[50%]">
+                  <label className="text-lg text-headingColor font-medium">
+                    Product Category*
+                  </label>
+                  <select
+                    className="py-[28px] pl-[20px] bg-[#D9D9D91A] rounded-[10px] border border-gray-400"
+                    defaultValue=""
+                  >
+                    <option value="" disabled>
+                      Product Category
+                    </option>
+                    <option value="Cannabis & CBD Packaging">
+                      Cannabis & CBD Packaging
+                    </option>
+                    <option value="Retail & Shopping Packaging">
+                      Retail & Shopping Packaging
+                    </option>
+                    <option value="Food & Beverage Packaging">
+                      Food & Beverage Packaging
+                    </option>
+                  </select>
+                </div>
+                {/*  */}
+                <div className="flex flex-col gap-y-[10.5px] w-[50%]">
+                  <label className="text-lg text-headingColor font-medium">
+                    Item Type*
+                  </label>
+                  <input
+                    className="py-[31px] pl-[49px] bg-[#D9D9D91A] rounded-[10px]"
+                    type="email"
+                    placeholder="I.e Perfume Bottle with Spray Top"
+                  />
+                </div>
+              </div>
+              {/* Second Input Column  */}
+              <div className="flex gap-10 mt-[19px]">
+                <div className="flex flex-col gap-y-[10.5px] w-[50%]">
+                  <label className="text-lg text-headingColor font-medium">
+                    Material(s) (optional)
+                  </label>
+                  <input
+                    className="py-[31px] pl-[49px] bg-[#D9D9D91A] rounded-[10px]"
+                    type="number"
+                    placeholder="Material(s)"
+                  />
+                </div>
+                {/*  */}
+                <div className="flex flex-col gap-y-[10.5px] w-[50%]">
+                  <label className="text-lg text-headingColor font-medium">
+                    Accessories (optional)
+                  </label>
+                  <input
+                    className="py-[31px] pl-[49px] bg-[#D9D9D91A] rounded-[10px]"
+                    type="text"
+                    placeholder="Accessories"
+                  />
+                </div>
+              </div>
+            </div>
+
+            {/* This is the contact information */}
+            <div className="">
+              {/* First Input Column */}
+              <div className="flex gap-10 mt-[19px]">
+                <div className="flex flex-col gap-y-[10.5px] w-[50%]">
+                  <label className="text-lg text-headingColor font-medium">
+                    Size*
+                  </label>
+                  <select
+                    className="py-[28px] pl-[20px] bg-[#D9D9D91A] rounded-[10px] border border-gray-400"
+                    defaultValue=""
+                  >
+                    <option value="" disabled>
+                      Select Size
+                    </option>
+                    <option value="Small">Small</option>
+                    <option value="Medium">Medium</option>
+                    <option value="Large">Large</option>
+                  </select>
+                </div>
+                {/*  */}
+                <div className="flex flex-col gap-y-[10.5px] w-[50%]">
+                  <label className="text-lg text-headingColor font-medium">
+                    Thickness in microns (optional)
+                  </label>
+                  <input
+                    className="py-[31px] pl-[49px] bg-[#D9D9D91A] rounded-[10px]"
+                    type="email"
+                    placeholder="Thickness in microns"
+                  />
+                </div>
+                {/*  */}
+                <div className="flex flex-col gap-y-[10.5px] w-[50%]">
+                  <label className="text-lg text-headingColor font-medium">
+                    Label?
+                  </label>
+                  {/*  */}
+                  <label class="switch">
+                    <input
+                      type="checkbox"
+                      checked={isToggled}
+                      onChange={handleToggle}
+                    />
+                    <span className="slider round">
+                      {isToggled ? "YES" : "NO"}
+                    </span>
+                  </label>
+                  {/* <input
+                    className="py-[31px] pl-[49px] bg-[#D9D9D91A] rounded-[10px]"
+                    type="email"
+                    placeholder="Email"
+                  /> */}
+                </div>
+              </div>
+            </div>
+
+            {/* This is the Order Quantity  */}
+            <div className="mt-12">
+              <h1 className="text-center text-buttonColor text-2xl">
+                Order Quantity
+              </h1>
+              {/* First Input Column */}
+              <div className="flex gap-10 mt-[19px]">
+                <div className="flex flex-col gap-y-[10.5px] w-[50%]">
+                  <label className="text-lg text-headingColor font-medium">
+                    Desired Quantity (required)*{" "}
+                    <span className="text-buttonColor text-lg font-semibold underline">
+                      MOQ guide
+                    </span>
+                  </label>
+                  <input
+                    className="py-[31px] pl-[49px] bg-[#D9D9D91A] rounded-[10px]"
+                    type="number"
+                    placeholder="Enter the number of units (e.g., 1,000)"
+                  />
+                </div>
+                {/*  */}
+                <div className="flex flex-col gap-y-[10.5px] w-[50%]">
+                  <label className="text-lg text-headingColor font-medium">
+                    Reoccurring?
+                  </label>
+                  <input
+                    className="py-[31px] pl-[49px] bg-[#D9D9D91A] rounded-[10px]"
+                    type="email"
+                    placeholder="Email"
+                  />
+                </div>
+              </div>
+            </div>
+
+            {/* This is the Shipping & Delivery */}
+            <div className="mt-12">
+              <h1 className="text-center text-buttonColor text-2xl">
+                Shipping & Delivery
+              </h1>
+              {/* First Input Column */}
+              <div className="mt-[19px]">
+                <div className="flex flex-col gap-y-[10.5px]">
+                  <label className="text-lg text-headingColor font-medium">
+                    Shipping Address (required)*
+                  </label>
+                  <input
+                    className="py-[31px] pl-[49px] bg-[#D9D9D91A] rounded-[10px]"
+                    type="text"
+                    placeholder="Enter full delivery address.."
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
+      </div>
+      {/* This it the button */}
+      <div className="flex justify-center mt-12">
+        <Button text={"Place Order"} color={"bg-buttonColor"} />
       </div>
     </section>
   );
