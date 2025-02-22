71ca88ca123d876980493eaf6a3de740227c830f
Shah Arefin Ahmed <shaharefinahmed@gmail.com>
2025-02-22 18:29:37 +0600

    Fix(spacing):Contact Us page spacing issue has been done

--- src/pages/ContactUs/ContactUs.jsx
+++ src/pages/ContactUs/ContactUs.jsx
@@ -3,7 +3,7 @@ import ContactForm from "@/components/ContactForm/ContactForm";
 const ContactUs = () => {
   return (
     <section className="xlg:pt-[122px] ">
-      <div className="container mx-auto">
+      <div className="container mx-auto px-5 lg:px-0">
         {/* This is the title section */}
         <div className="text-center">
           <h1 className="text-headingColor text-2xl lg:text-5xl font-bold">
