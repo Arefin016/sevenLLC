9ec8e2e16265a6b5819e673425a732c5b1b87f69
Shah Arefin Ahmed <shaharefinahmed@gmail.com>
2025-02-23 10:26:28 +0600

    Feat(responsive):Home page banner section responsive all device has been done

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,21 +1,22 @@
-import StepSection from '../../../components/StepSection/StepSection';
-import Banner from '../Banner/Banner';
-import Packaging from '../Packaging/Packaging';
-import Request from '../Request/Request';
-import Loader from '@/components/Loader/Loader';
-import ChooseUs from '../ChooseUs/ChooseUs';
-import BetterPlanet from '../BetterPlanet/BetterPlanet';
-import { betterPlanetData } from '@/data/data';
-import betterPlanetPic from '../../../assets/images/betterPlanetPic.png';
+import StepSection from "../../../components/StepSection/StepSection";
+import Banner from "../Banner/Banner";
+import Packaging from "../Packaging/Packaging";
+import Request from "../Request/Request";
+import Loader from "@/components/Loader/Loader";
+import ChooseUs from "../ChooseUs/ChooseUs";
+import BetterPlanet from "../BetterPlanet/BetterPlanet";
+import { betterPlanetData } from "@/data/data";
+import betterPlanetPic from "../../../assets/images/betterPlanetPic.png";
 import {
   useAllProductsQuery,
   useHomePageQuery,
   useHowItWorksQuery,
-} from '@/hooks/cms.queries';
+} from "@/hooks/cms.queries";
 
 const Home = () => {
   const { data: homepageData, isLoading } = useHomePageQuery();
-  const { data: allProducts, isLoading:productsLoading } = useAllProductsQuery();
+  const { data: allProducts, isLoading: productsLoading } =
+    useAllProductsQuery();
   const { data: howItWorksData, isLoading: worksLoading } =
     useHowItWorksQuery();
 
@@ -25,23 +26,23 @@ const Home = () => {
   return (
     <div>
       <Banner data={homepageData?.data?.hero_section} />
-      <Request />
-      <ChooseUs data={homepageData?.data?.why_choose_us} />
-      <Packaging data={allProducts} />
-      <div className="mt-[137px]">
+      {/* <Request /> */}
+      {/* <ChooseUs data={homepageData?.data?.why_choose_us} /> */}
+      {/* <Packaging data={allProducts} /> */}
+      {/* <div className="mt-[137px]">
         <StepSection
           data={howItWorksData}
-          title={'How It Works'}
-          subtitle={'Your Packaging, Simplified'}
-          btnText={'Learn More About Our Process'}
+          title={"How It Works"}
+          subtitle={"Your Packaging, Simplified"}
+          btnText={"Learn More About Our Process"}
         />
-      </div>
-      <div className="mt-[150px]">
+      </div> */}
+      {/* <div className="mt-[150px]">
         <BetterPlanet
           data={betterPlanetData}
           backgroundImage={betterPlanetPic}
         />
-      </div>
+      </div> */}
     </div>
   );
 };
